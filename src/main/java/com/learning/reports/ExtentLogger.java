691f87bfaade6f5ea0f7eeae8736f18349b4700a
“rajatt95” <rajatvermaa95@gmail.com>
2022-04-29 03:02:56 +0530

    Details added in files

--- src/main/java/com/learning/reports/ExtentLogger.java
+++ src/main/java/com/learning/reports/ExtentLogger.java
@@ -1,3 +1,13 @@
+/**
+ * @author Rajat Verma
+ * https://www.linkedin.com/in/rajat-v-3b0685128/
+ * https://github.com/rajatt95
+ * https://rajatt95.github.io/
+ *
+ * Course: Selenium - Java with Docker, Git and Jenkins (https://www.testingminibytes.com/courses/selenium-java-with-docker-git-and-jenkins/)
+ * Tutor: Amuthan Sakthivel (https://www.testingminibytes.com/)
+ */
+
 package com.learning.reports;
 
 import com.aventstack.extentreports.MediaEntityBuilder;
